68df8355c56b1900b8c24bea8ae5e942ed649112
denny <denny.george90@gmail.com>
2019-09-13 14:05:15 -0700

    added redirect code for squarespace website, ghost blog and services ui

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.redirect('https://gettattle.app/'))
+
+app.get('/blog', (req, res) => res.redirect('http://blog.tattle.co.in/'))
+
+app.get('/services', (req, res) => res.redirect('http://services.tattle.co.in/'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
